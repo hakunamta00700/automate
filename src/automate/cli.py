17e50136da763e2c48c24edb55ef8f716179ec73
David Cho <csi00700@gmail.com>
2025-05-02 14:21:03 +0900

    CLI 환경 설정에서 개발 및 프로덕션 환경에 따라 바인딩 주소를 수정함

--- src/automate/cli.py
+++ src/automate/cli.py
@@ -6,13 +6,15 @@ from automate.main import app
 
 def get_config(env: str) -> Config:
     config = Config()
-    config.bind = ["127.0.0.1:8000"]
+    
 
     if env == "dev":
+        config.bind = ["127.0.0.1:8000"]
         config.use_reloader = True
         config.reload_dir = "src"
         config.loglevel = "debug"
     elif env == "prod":
+        config.bind = ["0.0.0.0:8000"]
         config.workers = 4  # 멀티프로세스
         config.loglevel = "info"
         config.use_reloader = False
